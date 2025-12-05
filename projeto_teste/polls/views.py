8eb12e67cdd46bf5fe88a7551892e1aaed926a4a
Caio Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-12-05 13:26:17 +0000

    fixed 05/12

--- projeto_teste/polls/views.py
+++ projeto_teste/polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Post
-# Create your views here.
+
+
 
 def post_list(request):
-    posts = Post.objects.all()        
+    posts = Post.objects.filter(published_date__lte= timezone.now()).order_by('published_date')        
 
 
 
